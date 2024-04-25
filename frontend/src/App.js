a193219cb3433cbb7f66a07342f7bb6d1f7a752a
Flcable <flcable@gmail.com>
2024-04-25 04:01:21 +0100

    My first component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,14 @@
-function App() { const sn = 'Flavio Cruz'
-  return (<div><h1>Welcome to Bizza Plataform, {sn} !!!</h1></div>);
+import React from "react";
+import SpeakerProfile from "./SpeakerProfile";
+
+function App() {
+  const sn = 'Flavio Cruz'
+  return (
+    <div style={{ backgroundColor: 'gray', margin: 20, color: 'white' }}>
+      <SpeakerProfile />
+
+    </div>
+  );
 }
 
 export default App;
